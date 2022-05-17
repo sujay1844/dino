f13266e32bd73810b8308ce16b794d69c4c411b7
sujay1844 <sujay1844@protonmail.com>
2022-05-17 06:29:33 +0530

    Game now runs at 60fps

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import pygame
 WIDTH, HEIGHT = 800, 400
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game-lol")
+FPS = 60
 
 # Colours
 WHITE = (255, 255, 255)
+
 def main():
+    
+    clock = pygame.time.Clock()
+
     # running variable indicates whether the game is running or the user wants to quit
     running = True
     while running:
+        clock.tick(FPS)
+
         # Exiting the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,6 +28,7 @@ def main():
         
         # Update the game every frame
         pygame.display.update()
+
     
     pygame.quit()
 
