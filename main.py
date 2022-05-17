c745919b35c5423bf58c5266614ffaae9c16898c
sujay1844 <sujay1844@protonmail.com>
2022-05-17 06:14:48 +0530

    Window is drawn and we can quit it too

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main():
 
-    while True:
+    running = True
+    while running:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
+                running = False
     
     pygame.quit()
 
